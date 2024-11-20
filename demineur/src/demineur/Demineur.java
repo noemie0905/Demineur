8fa650a46acacacf6164957a834e3878bc540a80
phili <phili@10.3.62.177>
2024-11-20 14:50:26 +0100

    Création d'un nouveau projet versionné

---
+++ demineur/src/demineur/Demineur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demineur;
+
+/**
+ *
+ * @author phili
+ */
+public class Demineur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
